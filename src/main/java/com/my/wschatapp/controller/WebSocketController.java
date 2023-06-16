fb79e51fa8453f27945bdb9d9ff5e9666185da19
Konstantin <titarenko42k@gmail.com>
2023-06-16 11:19:20 +0300

    clean up

--- src/main/java/com/my/wschatapp/controller/WebSocketController.java
+++ src/main/java/com/my/wschatapp/controller/WebSocketController.java
@@ -3,7 +3,6 @@ package com.my.wschatapp.controller;
 import com.my.wschatapp.dto.ChatMessageDto;
 import com.my.wschatapp.dto.User;
 import com.my.wschatapp.utils.Utils;
-import org.slf4j.event.KeyValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
